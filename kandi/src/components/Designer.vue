9c1d3172f1c9141d119337a533c84e819ad36f0f
Giles-Aiden <agiles257@west-mec.org>
2021-09-29 13:16:37 -0700

    Replace DOM buttons with Canvas element to draw. Massive performace increase

--- kandi/src/components/Designer.vue
+++ kandi/src/components/Designer.vue
@@ -1,80 +1,14 @@
 <template>
-  <div
-    class="designer"
-    @pointerdown="onTouch($event, true)"
-    @pointerup="onTouch($event, false)"
-    @pointerleave="onTouch($event, false)"
-    @pointermove="onTouch($event)"
-  >
-    <div class="kandi perler" v-if="kandi.data != null && kandi.data.length > 0 && kandi.type == 'perler'">
-      <div class="column" v-for="(i, column) in kandi.data" :key="column">
-        <div class="row" v-for="(x, row) in i" :id="row" :key="row">
-          <button
-            :style="{ backgroundColor: x.color }"
-            class="button"
-            :data-col="column"
-            :data-row="row"
-          >
-            {{ i.length - row }}
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="kandi multi" v-else-if="kandi.data != null && kandi.data.length > 0 && kandi.type == 'multi'">
-      <div class="column" v-for="(i, column) in kandi.data" :key="column">
-        <div v-if="!(column % 2)">
-          <div class="row" v-for="(x, row) in i" :id="row" :key="row">
-            <button
-              :style="{ backgroundColor: x.color }"
-              class="button"
-              :data-col="column"
-              :data-row="row"
-            >
-              {{ multiCalc(i.length - row, false) }}
-            </button>
-          </div>
-        </div>
-        <div class="shift" v-else>
-          <div class="row" v-for="(x, row) in i" :id="row" :key="row">
-            <button
-              :style="{ backgroundColor: x.color }"
-              class="button"
-              :data-col="column"
-              :data-row="row"
-            >
-              {{ multiCalc(i.length - row, true) }}
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="kandi peyote" v-else-if="kandi.data != null && kandi.data.length > 0 && kandi.type == 'peyote'">
-      <div class="column" v-for="(i, column) in kandi.data" :key="column">
-        <div v-if="!(column % 2)">
-          <div class="row" v-for="(x, row) in i" :id="row" :key="row">
-            <button
-              :style="{ backgroundColor: x.color }"
-              class="button"
-              :data-col="column"
-              :data-row="row"
-            >
-              {{ multiCalc(i.length - row, false) }}
-            </button>
-          </div>
-        </div>
-        <div class="shift" v-else>
-          <div class="row" v-for="(x, row) in i" :id="row" :key="row">
-            <button
-              :style="{ backgroundColor: x.color }"
-              class="button"
-              :data-col="column"
-              :data-row="row"
-            >
-              {{ multiCalc(i.length - row, true) }}
-            </button>
-          </div>
-        </div>
-      </div>
+  <div class="designer">
+    <button @click="genCanvas">Generate Canvas</button>
+    <div class="canvas" v-if="props.kandi">
+      <canvas
+        id="canvas"
+        @pointermove="canvasTouch($event)"
+        >Your browser doesn't support the canvas tag. And that is genuinely
+        surprising, for the love of god how old is your browser? You should
+        update for your own safety.
+      </canvas>
     </div>
     <div v-else>
       <h2>Select your kandi measurements</h2>
@@ -83,7 +17,7 @@
 </template>
 
 <script setup>
-import { defineProps, defineExpose, computed } from "vue";
+import { computed, defineProps } from "vue";
 
 let props = defineProps({
   width: Number,
@@ -92,60 +26,68 @@ let props = defineProps({
   type: String,
   kandi: Object,
 });
+
 let kandi = computed(() => {
   return props.kandi;
 });
 
-let active = false;
-/*
-let kandi = reactive({
-  type: "",
-  title: "",
-  description: "",
-  author: "",
-  data: [],
-});
-*/
-// Calculates the numbers displayed on each button.
-// Takes an input number and bool stating if it's on an even or odd row
-const multiCalc = (val, even) => {
-  if (!even && val > 1) return val * 2 - 1;
-  else if (val > 1) return (val - 1) * 2;
-  else return val;
-};
+let size = 30,
+  border = 5,
+  canvasWidth = props.width * size + border * (props.width - 1),
+  canvasHeight = props.height * size + border * (props.height - 1);
 
-// Edits point data
-const editData = (x, y, color) => {
-  kandi.value.data[x][y].color = color;
-};
-
-const onTouch = (e, toggle) => {
-  if (toggle != undefined) active = toggle;
-  switch (e.pointerType) {
-    case "touch":
-      // If using the default target method, only the first target is registered with touch.
-      // Here we instead take the current touch location and try to find the element below that.
-      if (active && e.target.nodeName == "BUTTON") {
-        let ele = document.elementFromPoint(e.clientX, e.clientY);
-        editData(
-          ele.attributes["data-col"].value,
-          ele.attributes["data-row"].value,
-          props.selectedColor
-        );
-      }
-      break;
-    default:
-      if (active && e.target.nodeName == "BUTTON") {
-        editData(
-          e.target.attributes["data-col"].value,
-          e.target.attributes["data-row"].value,
-          props.selectedColor
-        );
-      }
+let genCanvas = () => {
+  let canvas = document.getElementById("canvas");
+  canvas.width = canvasWidth = props.width * size + border * (props.width - 1);
+  canvas.height = canvasHeight = props.height * size + border * (props.height - 1);
+  let ctx = canvas.getContext("2d");
+  for (let i = 0; i < kandi.value.data.length; i++) {
+    let point = kandi.value.data[i];
+    ctx.strokeStyle = "black";
+    ctx.fillStyle = point.color;
+    ctx.fillRect(
+      point.column * size + border * point.column,
+      point.row * size + border * point.row,
+      size,
+      size
+    );
   }
 };
 
-defineExpose({});
+//let canvasX, canvasY;
+let canvasTouch = (e) => {
+  if (e.buttons == 1) {
+    let canvas = document.getElementById("canvas");
+    let rect = canvas.getBoundingClientRect(), // abs. size of element
+      scaleX = canvasWidth / rect.width, // relationship bitmap vs. element for X
+      scaleY = canvasHeight / rect.height; // relationship bitmap vs. element for Y
+    let res = canvasToPoint(
+      (e.clientX - rect.left) * scaleX,
+      (e.clientY - rect.top) * scaleY
+    );
+    editCanvas(res[0], res[1]);
+  }
+};
+
+// Takes canvas location and converts it to kandi point data
+let canvasToPoint = (x, y) => {
+  let xRes = Math.floor(x / (size + border)),
+    yRes = Math.floor(y / (size + border));
+  return [xRes, yRes];
+};
+
+// Takes point data and writes it to kandi object, then redraws canvas
+let editCanvas = (x, y) => {
+  kandi.value.data.map((item) => {
+    if (x == item.column && y == item.row) {
+      item.color = props.selectedColor;
+    }
+  });
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  ctx.fillStyle = props.selectedColor;
+  ctx.fillRect(x * size + border * x, y * size + border * y, size, size);
+};
 </script>
 
 <style lang="scss">
@@ -154,60 +96,12 @@ defineExpose({});
  *  Multi - Vertical Shift
  *  Peyote - Horizontal Shift
 */
-.kandi.perler {
-  touch-action: none;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
-  .column {
-    display: flex;
-    flex-flow: column nowrap;
-    .row > button {
-      width: 2em;
-      height: 2em;
-      border: 1px solid black;
-      border-radius: 0.5em;
-      color: purple;
-    }
-  }
-}
-.kandi.multi {
-  touch-action: none;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
-  .column {
-    display: flex;
-    flex-flow: column nowrap;
-    .row > button {
-      width: 2em;
-      height: 2em;
-      border: 1px solid black;
-      border-radius: 0.5em;
-    }
-  }
-  .shift {
-    padding-top: 0.9em;
-  }
-}
-.kandi.peyote {
-  touch-action: none;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  .column > div {
+.designer {
+  .canvas {
     display: flex;
     flex-flow: row nowrap;
-    justify-content: center;
-    .row > button {
-      width: 2em;
-      height: 2em;
-      border: 1px solid black;
-      border-radius: 0.5em;
-    }
-  }
-  .shift {
-    padding-left: 1.5em;
+    justify-content: flex-start;
+    #canvas {}
   }
 }
 </style>
--- kandi/src/views/Maker.vue
+++ kandi/src/views/Maker.vue
@@ -1,42 +1,46 @@
 <template>
   <div class="maker">
     <div class="kandiData">
-      <select v-model="type">
-        <!-- 
-          Perler - Square and Equal
-          Multi - Vertical Shift
-          Peyote - Horizontal Shift
-        -->
-        <option value="perler">Perler - Square</option>
-        <option value="multi">Multi - Vertical</option>
-        <option value="peyote" selected>Peyote - Horizontal</option>
-      </select>
-      <input 
-        type="number"
-        v-model="width"
-        placeholder="columns"
-        min="1"
-        max="100"
-      />
-      <input
-        type="number"
-        v-model="height"
-        placeholder="rows"
-        min="1"
-        max="100"
-      />
-      <button type="button" @click="genData">Create!</button>
-      <br>`
-      <input id="color" type="color" v-model="color" />
+      <form @keyup.enter="genData">
+        <select v-model="type">
+          <!-- 
+            Perler - Square and Equal
+            Multi - Vertical Shift
+            Peyote - Horizontal Shift
+          -->
+          <option value="perler">Perler - Square</option>
+          <option value="ladder">Ladder - Square</option>
+          <option value="multi">Multi - Vertical</option>
+          <option value="peyote" selected>Peyote - Horizontal</option>
+        </select>
+        <input
+          type="number"
+          v-model="width"
+          placeholder="columns"
+          min="1"
+          max="100"
+        />
+        <input
+          type="number"
+          v-model="height"
+          placeholder="rows"
+          min="1"
+          max="100"
+        />
+        <input id="color" type="color" v-model="color" />
+        <button type="button" @click="genData">Create!</button>
+      </form>
     </div>
-    <Designer
-      class="designer"
-      :width="width"
-      :height="height"
-      :selectedColor="color"
-      :type="type"
-      :kandi="kandi"
-    />
+    <div class="designContain">
+      <Designer
+        class="designer"
+        :width="parseInt(width)"
+        :height="parseInt(height)"
+        :selectedColor="color"
+        :type="type"
+        :kandi="kandi"
+      />
+    </div>
   </div>
 </template>
 
@@ -46,7 +50,7 @@ import Designer from "@/components/Designer.vue";
 
 let height = ref();
 let width = ref();
-let color = ref("#FFFFFF")
+let color = ref("#FFFFFF");
 let type = ref("multi");
 
 /* Builds an Array of Arrays. Inside are objects containing point and color data.
@@ -58,14 +62,11 @@ let type = ref("multi");
  */
 let kandi = ref(new Object());
 const genData = () => {
-  console.log(kandi.value);
   kandi.value.type = type;
   kandi.value.data = [];
   for (let w = 0; w < width.value; w++) {
-    kandi.value.data.push([]);
-    kandi.value.data[w] = [];
     for (let h = 0; h < height.value; h++) {
-      kandi.value.data[w].push({
+      kandi.value.data.push({
         row: h,
         column: w,
         color: color.value,
@@ -80,6 +81,7 @@ const genData = () => {
   display: flex;
   justify-content: center;
   flex-flow: column nowrap;
+  overflow-x: hidden;
   .kandiData {
     display: flex;
     flex-flow: row wrap;
@@ -89,9 +91,10 @@ const genData = () => {
       flex-basis: 100%;
     }
   }
-  .designer {
-    max-width: 100%;
+  .designContain {
     overflow: auto;
+    border: 1px solid blue;
+    touch-action: none;
   }
 }
 </style>
